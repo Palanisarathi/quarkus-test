3c36dc3e90dae073b65cb5d015e6618ded99b988
quarkusio <no-reply@quarkus.io>
2024-06-22 16:00:21 +0000

    Initial commit

---
+++ src/main/java/org/quarkus/test/MyEntityResource.java
@@ -0,0 +1,6 @@
+package org.quarkus.test;
+
+import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
+
+public interface MyEntityResource extends PanacheEntityResource<MyEntity, Long> {
+}
